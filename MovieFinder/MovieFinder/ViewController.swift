471e2ff8b4598752eee9c130e50304b3b8797a6a
Selçuk ARIÖZ <107930904+selcukarz@users.noreply.github.com>
2023-10-27 14:48:45 +0300

    first commit

---
+++ MovieFinder/MovieFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Selçuk Arıöz on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
